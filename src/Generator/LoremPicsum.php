acccb858ee4553622d4dfcb6379e14b33fbb4064
Olivier Fouache <olivier.fouache@pictime-groupe.com>
2024-05-05 19:38:13 +0200

    update lorempicsum option

--- src/Generator/LoremPicsum.php
+++ src/Generator/LoremPicsum.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Olivier127\PlaceHolderGenerator\Generator;
 
+use Exception;
 use Olivier127\PlaceHolderGenerator\MediaFormat\Converter;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
@@ -19,13 +20,34 @@ class LoremPicsum implements PlaceHolderGeneratorInterface
 
     public function generate(string $suluMediaFormat, array $options = []): string
     {
+        if (array_key_exists('seed', $options) && array_key_exists('id', $this->config)) {
+            unset($this->config['id']);
+        }
+        if (array_key_exists('id', $options) && array_key_exists('seed', $this->config)) {
+            unset($this->config['seed']);
+        }
+        $config = array_merge($options, $this->config);
+        if (array_key_exists('id', $config) && array_key_exists('seed', $config)) {
+            throw new Exception('Seed or Id');
+        }
+
         $format = $this->converter->get($suluMediaFormat);
-        return sprintf(
-            "https://picsum.photos/%d/%d.%s?random=%s",
+
+        $url = sprintf(
+            "%d/%d.%s?random=%s",
             $format->width,
             $format->height,
             $format->extension,
             uniqid()
         );
+        $static = "";
+        if (array_key_exists('id', $config)) {
+            $static = sprintf("id/%d/", $config['id']);
+        }
+        if (array_key_exists('seed', $config)) {
+            $static = sprintf("seed/%s/", $config['seed']);
+        }
+
+        return "https://picsum.photos/".$static.$url;
     }
 }
